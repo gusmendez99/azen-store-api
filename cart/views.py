890271e2fb1feab09ddf895b374bc07898c6cd58
Gus Mendez <gus.mendez.99@gmail.com>
2020-05-24 22:19:36 -0600

    cart url with query param added

--- cart/views.py
+++ cart/views.py
@@ -34,6 +34,13 @@ class CartViewSet(viewsets.ModelViewSet):
         ),
     )
 
+    def get_queryset(self):
+        queryset = Cart.objects.all()
+        user_id = self.request.query_params.get('user', None)
+        if user_id is not None:
+            queryset = queryset.filter(user__id=user_id)
+        return queryset
+
     # POST method
     def perform_create(self, serializer):
         user = self.request.user
