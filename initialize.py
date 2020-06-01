e0e6e044c65ded706263fbfe714c4ae38f6087b3
Gus Mendez <gus.mendez.99@gmail.com>
2020-05-31 18:02:01 -0600

    gallery items added

--- initialize.py
+++ initialize.py
@@ -1,7 +1,8 @@
-""" import requests
+"""import requests
 from django.core.files import File
 from category.models import Category
-from product.models import Product """
+from product.models import Product
+from galleryitem.models import GalleryItem"""
 
 #Categories
 """ category1 = Category(name="Computadoras", icon="computer", description="Computadoras, Laptops")
@@ -173,4 +174,322 @@ product13.save()
 product13.categories.set([13])
 product13.featured_image.save("product-13.png", product13_file, save=True) """
 
+# Gallery Items
+
+""" product = Product.objects.filter(id=5).first()
+
+r = requests.get("https://images-na.ssl-images-amazon.com/images/I/811QpiYXe-L._AC_SL1500_.jpg")
+with open("/tmp/gallery-1.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-1.png", "rb")
+gallery1_file = File(reopen)
+gallery_item_1 = GalleryItem(product=product)
+gallery_item_1.save()
+gallery_item_1.image.save("gallery-1.png", gallery1_file, save=True)
+
+r = requests.get("https://geekzilla.tech/home/wp-content/uploads/2020/04/Screen-Shot-2020-04-08-at-12.58.29-PM.png")
+with open("/tmp/gallery-2.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-2.png", "rb")
+gallery2_file = File(reopen)
+gallery_item_2 = GalleryItem(product=product)
+gallery_item_2.save()
+gallery_item_2.image.save("gallery-2.png", gallery2_file, save=True)
+
+
+
+product = Product.objects.filter(id=6).first()
+
+r = requests.get("https://www.souqbaba.com/wp-content/uploads/2018/12/APPLE-MACBOOK-PRO-2018-MR9R2-SPACE-GREY-I5-8TH-GEN.-2.3-QUAD-CORE-8GB-512GB.png")
+with open("/tmp/gallery-3.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-3.png", "rb")
+gallery3_file = File(reopen)
+gallery_item_3 = GalleryItem(product=product)
+gallery_item_3.save()
+gallery_item_3.image.save("gallery-3.png", gallery3_file, save=True)
+
+r = requests.get("https://www.pclinks.com.bd/website/image/product.product/1562/image/2000x2000")
+with open("/tmp/gallery-4.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-4.png", "rb")
+gallery4_file = File(reopen)
+gallery_item_4 = GalleryItem(product=product)
+gallery_item_4.save()
+gallery_item_4.image.save("gallery-2.png", gallery4_file, save=True)
+
+
+
+
+
+product = Product.objects.filter(id=7).first()
+
+r = requests.get("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDAwxQ363OHCv8hwLG6k4qUHV5Z6d4Rghz5q2zxVdmhQkTCnde&usqp=CAU")
+with open("/tmp/gallery-5.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-5.png", "rb")
+gallery5_file = File(reopen)
+gallery_item_5 = GalleryItem(product=product)
+gallery_item_5.save()
+gallery_item_5.image.save("gallery-5.png", gallery5_file, save=True)
+
+r = requests.get("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR3F5uHDS0trmscIAORJF9144HbV-i_bZxlLRqOO0eFqB0VRkPl&usqp=CAU")
+with open("/tmp/gallery-6.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-6.png", "rb")
+gallery6_file = File(reopen)
+gallery_item_6 = GalleryItem(product=product)
+gallery_item_6.save()
+gallery_item_6.image.save("gallery-6.png", gallery6_file, save=True)
+
+
+
+
+
+
+product = Product.objects.filter(id=8).first()
+
+r = requests.get("https://static.kemikcdn.com/2017/06/sa400s37-120gb-422x571.jpg")
+with open("/tmp/gallery-7.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-7.png", "rb")
+gallery7_file = File(reopen)
+gallery_item_7 = GalleryItem(product=product)
+gallery_item_7.save()
+gallery_item_7.image.save("gallery-7.png", gallery7_file, save=True)
+
+r = requests.get("https://media.kingston.com/kingston/product/ktc-product-ssd-a400-sa400s37-120gb-1-sm.jpg")
+with open("/tmp/gallery-8.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-8.png", "rb")
+gallery8_file = File(reopen)
+gallery_item_8 = GalleryItem(product=product)
+gallery_item_8.save()
+gallery_item_8.image.save("gallery-8.png", gallery8_file, save=True)
+
+
+
+
+
+
+product = Product.objects.filter(id=9).first()
+
+r = requests.get("https://www.cyberpuerta.mx/img/product/L/CP-HYPERX-HX432C16PB3A8-3.jpg")
+with open("/tmp/gallery-9.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-9.png", "rb")
+gallery9_file = File(reopen)
+gallery_item_9 = GalleryItem(product=product)
+gallery_item_9.save()
+gallery_item_9.image.save("gallery-9.png", gallery9_file, save=True)
+
+r = requests.get("https://www.cyberpuerta.mx/img/product/L/CP-HYPERX-HX432C16PB3A8-6.jpg")
+with open("/tmp/gallery-10.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-10.png", "rb")
+gallery10_file = File(reopen)
+gallery_item_10 = GalleryItem(product=product)
+gallery_item_10.save()
+gallery_item_10.image.save("gallery-10.png", gallery10_file, save=True)
+
+
+
+
+
+
+product = Product.objects.filter(id=10).first()
+
+r = requests.get("https://cdn.cnetcontent.com/bf/5f/bf5f8232-afbe-412e-b8bf-e204bdc9173c.jpg")
+with open("/tmp/gallery-11.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-11.png", "rb")
+gallery11_file = File(reopen)
+gallery_item_11 = GalleryItem(product=product)
+gallery_item_11.save()
+gallery_item_11.image.save("gallery-11.png", gallery11_file, save=True)
+
+r = requests.get("https://www.cyberpuerta.mx/img/product/L/CP-DELL-210-AQCS-6.jpg")
+with open("/tmp/gallery-12.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-12.png", "rb")
+gallery12_file = File(reopen)
+gallery_item_12 = GalleryItem(product=product)
+gallery_item_12.save()
+gallery_item_12.image.save("gallery-12.png", gallery12_file, save=True)
+
+
+
+
+
+
+
+product = Product.objects.filter(id=11).first()
+
+r = requests.get("https://c1.neweggimages.com/NeweggImage/ProductImage/26-197-252-Z01.jpg")
+with open("/tmp/gallery-13.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-13.png", "rb")
+gallery13_file = File(reopen)
+gallery_item_13 = GalleryItem(product=product)
+gallery_item_13.save()
+gallery_item_13.image.save("gallery-13.png", gallery13_file, save=True)
+
+r = requests.get("https://hardwareviews.com/wp-content/uploads/2020/04/logitech-g203-lightsync.jpg")
+with open("/tmp/gallery-14.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-14.png", "rb")
+gallery14_file = File(reopen)
+gallery_item_14 = GalleryItem(product=product)
+gallery_item_14.save()
+gallery_item_14.image.save("gallery-14.png", gallery14_file, save=True)
+
+
+
+
+
+product = Product.objects.filter(id=12).first()
+
+r = requests.get("https://www.macrosistemas.com/images/virtuemart/product/teclado-18.jpg")
+with open("/tmp/gallery-15.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-15.png", "rb")
+gallery15_file = File(reopen)
+gallery_item_15 = GalleryItem(product=product)
+gallery_item_15.save()
+gallery_item_15.image.save("gallery-15.png", gallery15_file, save=True)
+
+r = requests.get("https://resources.claroshop.com/medios-plazavip/mkt/5ca62e799ad2b_4jpg.jpg")
+with open("/tmp/gallery-16.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-16.png", "rb")
+gallery16_file = File(reopen)
+gallery_item_16 = GalleryItem(product=product)
+gallery_item_16.save()
+gallery_item_16.image.save("gallery-16.png", gallery16_file, save=True)
+
+
+
+
+
+
+product = Product.objects.filter(id=13).first()
+
+r = requests.get("https://cdn.lumingo.com/medias/0100124913-000000000004649641-0-c515Wx515H?context=bWFzdGVyfGltYWdlc3wxODkzNHxpbWFnZS9qcGVnfGltYWdlcy9oN2YvaDQ3LzkwOTI5NzUxOTgyMzguanBnfGZhYTY4MTg1YTAzZDhiMmZlODhmNDA0NjczNGE5NDZlNWRhOTk1MmYzMzEwZTY3NTc5ZDI4NmI3N2MzZDE3YTU")
+with open("/tmp/gallery-17.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-17.png", "rb")
+gallery17_file = File(reopen)
+gallery_item_17 = GalleryItem(product=product)
+gallery_item_17.save()
+gallery_item_17.image.save("gallery-17.png", gallery17_file, save=True)
+
+r = requests.get("https://resources.claroshop.com/medios-plazavip/s2/10180/1221795/5da3dc0a5427f-2_un50ru7100-1600x1600.jpg")
+with open("/tmp/gallery-18.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-18.png", "rb")
+gallery18_file = File(reopen)
+gallery_item_18 = GalleryItem(product=product)
+gallery_item_18.save()
+gallery_item_18.image.save("gallery-18.png", gallery18_file, save=True)
+
+
+
+
+
+
+
+product = Product.objects.filter(id=14).first()
+
+r = requests.get("https://m.media-amazon.com/images/I/71eSPhz0IjL.jpg")
+with open("/tmp/gallery-19.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-19.png", "rb")
+gallery19_file = File(reopen)
+gallery_item_19 = GalleryItem(product=product)
+gallery_item_19.save()
+gallery_item_19.image.save("gallery-19.png", gallery19_file, save=True)
+
+r = requests.get("https://static.kemikcdn.com/2016/04/0dd8c853-3f34-4f58-a360-b076e6db3d8f.jpg")
+with open("/tmp/gallery-20.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-20.png", "rb")
+gallery20_file = File(reopen)
+gallery_item_20 = GalleryItem(product=product)
+gallery_item_20.save()
+gallery_item_20.image.save("gallery-20.png", gallery20_file, save=True)
+
+
+
+
+
+product = Product.objects.filter(id=15).first()
+
+r = requests.get("https://www.tiendacanon.com.mx/wcsstore/CMEXCatalogAssetStore/G1110_AS_FR_cl_xl.jpg")
+with open("/tmp/gallery-21.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-21.png", "rb")
+gallery21_file = File(reopen)
+gallery_item_21 = GalleryItem(product=product)
+gallery_item_21.save()
+gallery_item_21.image.save("gallery-21.png", gallery21_file, save=True)
+
+r = requests.get("https://www.intelcomphonduras.com/wp-content/uploads/2018/10/impresora-canon-g1110-pixmainyeccion-de-tinta-D_NQ_NP_863784-MRD27406393430_052018-F.jpg")
+with open("/tmp/gallery-22.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-22.png", "rb")
+gallery22_file = File(reopen)
+gallery_item_22 = GalleryItem(product=product)
+gallery_item_22.save()
+gallery_item_22.image.save("gallery-22.png", gallery22_file, save=True)
+
+
+
+
+
+product = Product.objects.filter(id=16).first()
+
+r = requests.get("https://easetec.com.pk/wp-content/uploads/2019/05/91Qg5K-cmfL._SL1500_.jpg")
+with open("/tmp/gallery-23.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-23.png", "rb")
+gallery23_file = File(reopen)
+gallery_item_23 = GalleryItem(product=product)
+gallery_item_23.save()
+gallery_item_23.image.save("gallery-23.png", gallery23_file, save=True)
+
+r = requests.get("https://c1.neweggimages.com/ProductImage/14-137-338-V19.jpg")
+with open("/tmp/gallery-24.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-24.png", "rb")
+gallery24_file = File(reopen)
+gallery_item_24 = GalleryItem(product=product)
+gallery_item_24.save()
+gallery_item_24.image.save("gallery-24.png", gallery24_file, save=True)
+
+
+
+
+
+
+
+
+product = Product.objects.filter(id=17).first()
+
+r = requests.get("https://my.accvent.com/images/stories/virtuemart/product/xtf-cd4333.png")
+with open("/tmp/gallery-25.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-25.png", "rb")
+gallery25_file = File(reopen)
+gallery_item_25 = GalleryItem(product=product)
+gallery_item_25.save()
+gallery_item_25.image.save("gallery-25.png", gallery25_file, save=True)
+
+r = requests.get("https://www.xtechamericas.com/media/XTF-CD433-002.jpg")
+with open("/tmp/gallery-26.png", "wb") as f:
+    f.write(r.content)
+reopen = open("/tmp/gallery-26.png", "rb")
+gallery26_file = File(reopen)
+gallery_item_26 = GalleryItem(product=product)
+gallery_item_26.save()
+gallery_item_26.image.save("gallery-26.png", gallery26_file, save=True) """
 
